0786dc31bfb677bd6aabad5721a67afb8928a72f
lixiankun <1263930893@qq.com>
2020-08-23 21:50:52 +0800

    [week6]作业提交

--- Week_06/README.md
+++ Week_06/README.md
@@ -1,1 +1,28 @@
 学习笔记
+将复杂问题分解成子问题 并寻找重复性
+递归：
+```
+void recur(int level,int param)
+ if(level > MAX_LEVEL)
+    return
+ procces(level,param);
+ recr(level:level+1,newparam);
+ //restore curent status
+```
+
+分治：(递归的特殊形式)
+void divide_conquer(problem,parm1,param2,param3...)
+    if problem is None
+        print_result
+        return
+    data = prepare_data(prblem)
+    subproblems = split_problem(problem,data)
+    
+    subResult1 = divide_conquer(subproblem[0],p1,...)
+    subResult2 = divide_conquer(subproblem[1],p1,...)
+    ...
+    result = process_result(subresult1,subreslut2...)
+    //revert the current level states
+回溯：DFS
+贪心：只看眼前最优解，结果不一定是全局最优解
+动态规划：分支 + 最优子结构（记录最优解，中途进行淘汰次优解）
---
+++ Week_06/myweek6.go
@@ -0,0 +1,74 @@
+//最小路径和
+class Solution {
+public:
+void printAll(vector<int> &result)
+{
+	for(int i=0;i<result.size();i++)
+	{
+		printf("%d ",result[i]);
+	}
+	printf("\n");
+}
+int minPathSum(vector<vector<int>>& grid) {
+//dp[i,j] = min(dp[i-1,j],dp[i,j-1]) + nums[i,j];
+//dp[0,0] = 1;dp[0,1] = 4;dp[0,2]=5
+//dp[1,0] = dp[0,0] + nums[i,j] = 2,dp[1][1]=min(dp[0,1],dp[1,0])=7
+if(grid.size() <= 0 || grid[0].size() <=0 ) return -1;
+int m= grid.size(),n=grid[0].size();
+vector<int> res(n);
+
+res[0] = grid[0][0];
+for(int k=1;k<n;k++)
+{
+	res[k] = res[k-1] + grid[0][k];
+}
+printAll(res);//[1,3]
+for(int i=1;i<m;i++)
+{
+	for(int j=0;j<n;j++)
+	{
+		int min = res[j];
+		if(j-1 >= 0)
+		{
+			min = min > res[j-1] ? res[j-1]:min;
+		}
+		res[j] = min + grid[i][j];
+	}
+//printAll(res);
+	}
+	return res[n-1];
+	}
+};
+
+//最大正方形
+class Solution {
+public int maximalSquare(char[][] matrix) {
+int n = matrix.length;
+if(n == 0) return 0;
+int m = matrix[0].length;
+if(m == 0) return 0;
+int[][] dp = new int[n][m];
+int result = 0;
+//初始化base case
+for(int i = 0 ;i<n;i++) {
+dp[i][0] = matrix[i][0] - '0';
+result = Math.max(dp[i][0],result);
+}
+for(int i = 0 ;i<m;i++){
+dp[0][i] = matrix[0][i] - '0';
+result = Math.max(dp[0][i],result);
+}
+for(int i = 1; i<n; i++){
+for(int j = 1; j<m; j++){
+//计算dp[i][j];
+if(matrix[i][j] == '1'){
+dp[i][j] = Math.min(Math.min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1]) + 1;
+result = Math.max(dp[i][j],result);
+}
+
+}
+}
+return result*result;
+
+}
+}
